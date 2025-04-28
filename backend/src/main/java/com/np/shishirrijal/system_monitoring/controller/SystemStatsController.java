0254612c666be84057543c75ef2bef2894dbad11
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-29 03:27:01 +0545

    add cors

--- backend/src/main/java/com/np/shishirrijal/system_monitoring/controller/SystemStatsController.java
+++ backend/src/main/java/com/np/shishirrijal/system_monitoring/controller/SystemStatsController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SystemStatsController{
 
     private final SystemSnapshotRepository snapshotRepository;
